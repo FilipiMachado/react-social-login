08e97723bd3e3d867d0eca2b4ad322ea41c3c304
Filipi <filipisee@hotmail.com>
2021-11-23 19:03:01 -0300

    adding Home page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,10 +2,11 @@ import React from "react";
 
 const Card = ({ post }) => {
   return (
-    <div>
+    <div className="card">
       <span className="card-title">{post.title}</span>
       <img src={post.img} alt="" className="card-img" />
       <p className="card-desc">{post.desc}</p>
+      <button className="card-button">Read More</button>
     </div>
   );
 };
