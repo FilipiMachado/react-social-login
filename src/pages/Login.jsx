2868522b6f45306599dc0ffc29db2748a2a1cdd5
Filipi <filipisee@hotmail.com>
2021-11-23 19:42:25 -0300

    added login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+// Icons
+import Facebook from "../img/facebook.png";
+import Github from "../img/github.png";
+import Google from "../img/google.png";
 
 const Login = () => {
   return (
     <div className="login">
       <h1 className="login-title">Choose a Login Method</h1>
       <div className="wrapper">
-        <div className="left"></div>
+        <div className="left">
+          <div className="login-button google">
+            <img className="icon" src={Facebook} alt="facebook icon" />
+            Facebook
+          </div>
+          <div className="login-button github">
+            <img className="icon" src={Github} alt="github icon" />
+            Github
+          </div>
+          <div className="login-button google">
+            <img className="icon" src={Google} alt="google icon" />
+            Google
+          </div>
+        </div>
         <div className="right"></div>
       </div>
     </div>
