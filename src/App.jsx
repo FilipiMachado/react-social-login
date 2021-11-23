90ab8e0b1a235f8a667aa89083c505581d984a01
Filipi <filipisee@hotmail.com>
2021-11-23 18:45:08 -0300

    adding Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // Components
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 //Styles
 import "./App.css";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Home />
     </>
   );
 }
