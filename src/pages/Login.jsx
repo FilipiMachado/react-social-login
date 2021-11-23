7fba9cb3324853b331dbcacc19d726a942469c34
Filipi <filipisee@hotmail.com>
2021-11-23 19:37:20 -0300

    added login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
-    return (
-        <div>
-            login
-        </div>
-    )
-}
+  return (
+    <div className="login">
+      <h1 className="login-title">Choose a Login Method</h1>
+      <div className="wrapper">
+        <div className="left"></div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
