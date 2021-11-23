7fd0c2db78fdb3a9bb3069f064870681937d4a63
Filipi <filipisee@hotmail.com>
2021-11-23 18:37:13 -0300

    styling header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,11 @@ const Navbar = () => {
       <span className="logo">Social Login</span>
       <ul className="list">
         <li className="list-item">
-          <img className="avatar" src="" alt="" />
+          <img
+            className="avatar"
+            src="https://images.pexels.com/photos/9737563/pexels-photo-9737563.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+            alt="avatar"
+          />
         </li>
         <li className="list-item">Fil Machado</li>
         <li className="list-item">Logout</li>
