7fd229076c06f0d3e8bd3f8d82ef1f32a208d984
Filipi <filipisee@hotmail.com>
2021-11-23 18:24:55 -0300

    styling header

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// Components
+import Navbar from "./components/Navbar";
+//Styles
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <span className="logo">Social Login</span>
+      <ul className="list">
+        <li className="list-item">
+          <img className="avatar" src="" alt="" />
+        </li>
+        <li className="list-item">Fil Machado</li>
+        <li className="li list-item">Logout</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
