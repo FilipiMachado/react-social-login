6cad4e32c3f71239cf0ad8a5e28f1eef1bbae25e
Filipi <filipisee@hotmail.com>
2021-11-25 19:39:20 -0300

    adding routes

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+// React Router
+import { Link } from "react-router-dom";
 
 const Card = ({ post }) => {
   return (
     <div className="card">
-      <span className="card-title">{post.title}</span>
-      <img src={post.img} alt="" className="card-img" />
-      <p className="card-desc">{post.desc}</p>
-      <button className="card-button">Read More</button>
+      <Link className="link" to={`/post/${post.id}`}>
+        <span className="card-title">{post.title}</span>
+        <img src={post.img} alt="" className="card-img" />
+        <p className="card-desc">{post.desc}</p>
+        <button className="card-button">Leia Mais</button>
+      </Link>
     </div>
   );
 };
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+// React Router
 import { Link } from "react-router-dom";
 
 const Navbar = ({ user }) => {
