06d58234d85ea36757489d55f15a7b096d1c6427
Filipi <filipisee@hotmail.com>
2021-11-25 20:10:38 -0300

    added passport cors cookies

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,26 @@
+// Express
 const express = require("express");
 const app = express();
+// Cookie Session
+const cookieSession = require("cookie-session");
+
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["fil"],
+    maxAge: 24 * 60 * 60 * 100,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.listen("5000", () => {
-  console.log("Server is running!");
+  console.log("Server is running!!!");
 });
