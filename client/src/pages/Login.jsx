1f8b96ff4fd0408672ee56fd51157d8e674c1ae4
Filipi <filipisee@hotmail.com>
2021-11-25 16:15:51 -0300

    changed title

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -7,7 +7,7 @@ import Google from "../img/google.png";
 const Login = () => {
   return (
     <div className="login">
-      <h1 className="login-title">Choose a Login Method</h1>
+      <h1 className="login-title">Escolha um Método de Login</h1>
       <div className="wrapper">
         <div className="left">
           <div className="login-button facebook">
@@ -25,7 +25,7 @@ const Login = () => {
         </div>
         <div className="center">
           <div className="line" />
-          <div className="or">OR</div>
+          <div className="or">OU</div>
         </div>
         <div className="right">
           <input type="text" placeholder="Username" />
